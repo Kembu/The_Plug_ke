26b6f63bc9badf0feafe2a2db9d6079a80ef6b7b
karreycodes <kamondewangari@gmail.com>
2023-09-19 22:36:18 +0300

    changes to log in and sign up

--- the_plug/src/pages/Login.js
+++ the_plug/src/pages/Login.js
@@ -1,116 +1,147 @@
-import React, { Component } from 'react';
-import { StyleSheet, css, } from 'aphrodite';
+import React, { Component } from "react";
+import { StyleSheet, css } from "aphrodite";
 
 const styles = StyleSheet.create({
-  container: {
-    textAlign: 'center',
-    background: 'black',
-    borderRadius: '40px',
-    borderTop: '10px solid #f370b8',
-    borderBottom: '10px solid #f370b8',
-    width: '400px',
-    height: '500px',
+  body: {
+    fontFamily: '"Source Sans Pro", sans-serif',
+    background: 'url("./background.jpg")',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    color: "#ffffff",
+    fontSize: "20px",
+    margin: "0",
+    minHeight: "100vh", // Ensure the body covers the entire viewport height
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+
+  container: {
+    display: "flex",
+    alignItems: "center",
+    background: "black",
+    borderRadius: "50px",
+    borderTop: "10px solid #f370b8",
+    borderBottom: "10px solid #f370b8",
+    width: "410px",
+    height: "500px",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+
   box: {
     fontFamily: '"Source Sans Pro", sans-serif',
-    color: '#ffffff',
-    padding: '20px',
-    margin: '0 auto',
-    width: '320px',
+    color: "#ffffff",
+    padding: "20px",
+    margin: "0 auto",
+    width: "330px",
   },
   h4: {
-    fontSize: '20px',
-    marginTop: '94px',
-    color: '#f370b8',
+    fontSize: "30px",
+    textAlign: "center", // Center horizontally
+    marginTop: "10px",
+    color: "#f370b8",
   },
   h5: {
     fontFamily: '"Source Sans Pro", sans-serif',
-    fontSize: '13px',
-    color: '#ffffff',
-    letterSpacing: '1.5px',
-    marginTop: '-15px',
-    marginBottom: '70px',
+    fontSize: "15px",
+    color: "#ffffff",
+    letterSpacing: "1.5px",
+    margin: "0", // Remove marginTop and marginBottom
+    display: "flex",
+    alignItems: "center", // Center vertically
+    justifyContent: "center", // Center horizontally
+    height: "100%", // Ensure the container takes up the full height
   },
+
   input: {
-    display: 'block',
-    margin: '20px auto',
-    backgroundColor: '#3b4148',
-    border: '0',
-    borderRadius: '5px',
-    padding: '14px 10px',
-    width: '320px',
-    outline: '0',
-    color: '#ffffff',
-    transition: 'all 0.2s ease-out',
-    '::placeholder': {
-      color: '#ffffff',
+    display: "block",
+    margin: "20px auto",
+    backgroundColor: "#3b4148",
+    border: "0",
+    borderRadius: "5px",
+    padding: "14px 10px",
+    width: "320px",
+    outline: "0",
+    color: "#ffffff",
+    transition: "all 0.2s ease-out",
+    "::placeholder": {
+      color: "#ffffff",
     },
-    ':focus': {
-      border: '1px solid #79a6fe',
+    ":focus": {
+      border: "1px solid #79a6fe",
     },
   },
   a: {
-    color: '#f370b8',
-    textDecoration: 'none',
-    ':hover': {
-      color: '#129aa1',
+    color: "#f370b8",
+    textDecoration: "none",
+    ":hover": {
+      color: "#129aa1",
     },
   },
   btn1: {
-    border: '0',
-    background: '#f370b8',
-    color: '#dfdeee',
-    borderRadius: '7px',
-    width: '340px',
-    height: '49px',
-    fontSize: '16px',
-    transition: '0.3s',
-    cursor: 'pointer',
-    marginTop: '20px',
-    ':hover': {
-      background: '#129aa1',
+    border: "0",
+    background: "#f370b8",
+    color: "#dfdeee",
+    borderRadius: "10px",
+    width: "340px",
+    height: "49px",
+    fontSize: "16px",
+    transition: "0.3s",
+    cursor: "pointer",
+    marginTop: "20px",
+    ":hover": {
+      background: "#129aa1",
     },
   },
   forgetpass: {
-    position: 'relative',
-    float: 'right',
-    right: '28px',
+    // position: 'right',
+    float: "right",
+    // right: '30px',
   },
 });
 
 class Login extends Component {
   componentDidMount() {
-    document.title = 'Login Page'; 
+    document.title = "Login Page";
   }
   render() {
     return (
-      <div className={css(styles.container)}>
-        <form name="form1" className={css(styles.box)} method="post">
-          <h4 className={css(styles.h4)}>Login</h4>
-          <h5 className={css(styles.h5)}>Sign in to your account.</h5>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            autoComplete="off"
-            className={css(styles.input)}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            id="pwd"
-            autoComplete="off"
-            className={css(styles.input)}
-          />
-          <a href="#" className={css(styles.forgetpass)}>
-            Forgot Password?
-          </a>
-          <input type="submit" value="Log In" className={css(styles.btn1)} />
-          <p>
-            Don't have an account? <a href="signup.html" className={css(styles.a)}>Sign Up</a>
-          </p>
-        </form>
+      <div className={css(styles.body)}>
+        <div className={css(styles.container)}>
+          <form name="form1" className={css(styles.box)} method="post">
+            <h4 className={css(styles.h4)}>Login</h4>
+            <h5 className={css(styles.h5)}>Sign in to your account.</h5>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              autoComplete="off"
+              className={css(styles.input)}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="pwd"
+              autoComplete="off"
+              className={css(styles.input)}
+            />
+            <a href="#" className={css(styles.forgetpass)}>
+              Forgot Password?
+            </a>
+            <input type="submit" value="Log In" className={css(styles.btn1)} />
+            <p>
+              Don't have an account?{" "}
+              <a href="signup" className={css(styles.a)}>
+                Sign Up
+              </a>
+            </p>
+          </form>
+        </div>
       </div>
     );
   }
--- the_plug/src/pages/SignUp.js
+++ the_plug/src/pages/SignUp.js
@@ -1,42 +1,123 @@
-import React, { Component } from 'react';
-import { StyleSheet, css } from 'aphrodite';
+import React, { Component } from "react";
+import { StyleSheet, css } from "aphrodite";
 
 // Define your CSS styles using Aphrodite
 const styles = StyleSheet.create({
   body: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    overflow: 'hidden',
-  },
-  container: {
-    textAlign: 'center',
-    background: 'black',
-    borderRadius: '50px',
-    borderTop: '10px solid #f370b8',
-    borderBottom: '10px solid #f370b8',
-    width: '400px',
-  },
-  box: {
     fontFamily: '"Source Sans Pro", sans-serif',
-    color: '#ffffff',
-    fontSize: '20px',
+    background: 'url("./background.jpg")',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    color: "#ffffff",
+    fontSize: "20px",
+    margin: "0",
+    minHeight: "100vh", // Ensure the body covers the entire viewport height
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  // Add more styles here as needed
+
+  container: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    textAlign: "center",
+    background: "black",
+    borderRadius: "70px",
+    borderTop: "10px solid #f370b8",
+    borderBottom: "10px solid #f370b8",
+    width: "400px",
+    padding: "20px",
+    height: "500px",
+    boxSizing: "border-box",
+  },
+
+  boxH4: {
+    fontFamily: '"Source Sans Pro", sans-serif',
+    color: "#f370b8",
+    fontSize: "40px",
+    marginTop: "100px",
+  },
+
+  boxH5: {
+    fontFamily: '"Source Sans Pro", sans-serif',
+    fontSize: "10px",
+    color: "#ffffff",
+    letterSpacing: "1.5px",
+    marginTop: "-80px",
+    marginBottom: "70px",
+  },
+
+  boxInput: {
+    display: "block",
+    margin: "20px auto",
+    backgroundColor: "#3b4148",
+    border: "0",
+    borderRadius: "5px",
+    padding: "14px 10px",
+    width: "100%",
+    outline: "0",
+    color: "#ffffff",
+    transition: "all 0.2s ease-out",
+  },
+
+  inputPlaceholder: {
+    color: "#ffffff",
+  },
+
+  boxInputFocus: {
+    border: "1px solid #79a6fe",
+  },
+
+  fullNameInput: {
+    marginTop: "40px",
+  },
+
+  link: {
+    color: "#f370b8",
+    textDecoration: "none",
+  },
+
+  linkHover: {
+    color: "#129aa1",
+  },
+
+  btn1: {
+    border: "0",
+    background: "#f370b8",
+    color: "#dfdeee",
+    borderRadius: "7px",
+    width: "340px",
+    height: "49px",
+    fontSize: "16px",
+    transition: "0.3s",
+    cursor: "pointer",
+    marginTop: "20px",
+  },
+
+  btn1Hover: {
+    background: "#129aa1",
+  },
+
+  // forgetPass: {
+  //   position: 'relative',
+  //   float: 'right',
+  //   right: '28px',
+  // },
 });
 
 class SignUp extends Component {
   componentDidMount() {
-    document.title = 'Sign Up'; 
+    document.title = "Sign Up";
   }
   render() {
     return (
       <div className={css(styles.body)}>
         <div className={css(styles.container)}>
           <form name="form1" className={css(styles.box)} method="post">
-            <h4 className={css(styles.box, styles.h4)}>Sign Up</h4>
-            <h5 className={css(styles.box, styles.h5)}>Create your account.</h5>
+            <h4 className={css(styles.boxH4)}>Sign Up</h4>
+            <h5 className={css(styles.boxH5)}>Create your account.</h5>
             <input
               type="text"
               name="name"
@@ -57,15 +138,14 @@ class SignUp extends Component {
               placeholder="Password"
               id="pwd"
               autoComplete="off"
-              className={css(styles.box, styles.input)}
+              className={css(styles.forgetPass)}
             />
-            <input
-              type="submit"
-              value="Sign Up"
-              className={css(styles.box, styles.btn1)}
-            />
+            <input type="submit" value="Sign Up" className={css(styles.btn1)} />
             <p>
-              Already have an account? <a href="login.html">Log In</a>
+              Already have an account?{" "}
+              <a href="http://localhost:3000/login" className="link">
+                Log In
+              </a>
             </p>
           </form>
         </div>
